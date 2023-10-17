30f2f0ceca6799200499c5cf8f2c2780df078a99
changhoji <onkkno1@gmail.com>
2023-10-17 09:24:18 +0900

    init

---
+++ SwiftUIWithFirestore/SwiftUIWithFirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithFirestoreApp.swift
+//  SwiftUIWithFirestore
+//
+//  Created by changhoji on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithFirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
